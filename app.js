dcd4a0f42e30182a225a525d4ef374bea9a6fb9a
KevinLZC <klazaro0@ucol.mx>
2022-11-04 12:55:33 -0600

    Métodos avance

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ class Base {
 		this.sig = null;
 		this.ant = null;
 	}
+
+	descripcion() {
+		return `Nombre: ${this.nombre} - Minutos: ${this.minutos}\n`;
+	}
+
 	getNombre() {
 		return this.nombre;
 	}
@@ -18,9 +23,87 @@ class Ruta {
 		this.primero = null;
 	}
 
-	agregar(nuevo) {}
-	buscar(nombre) {}
-	eliminar(nombre) {}
-	imprimir() {}
-	recorrido(baseInicio, horaInicio, minutoInicio, horaFin, minutoFin) {}
+	agregar(nuevo) {
+		if(this.primero === null) {
+			this.primero = nuevo;
+			nuevo.sig = nuevo;
+			nuevo.ant = nuevo;
+		} else if(this.primero.sig === this.primero){
+			this.primero.sig = nuevo;
+			this.primero.ant = nuevo;
+			nuevo.sig = this.primero;
+			nuevo.ant = this.primero;
+		} else {
+			nuevo.sig = this.primero;
+			nuevo.ant = this.primero.ant;
+			this.primero.ant.sig = nuevo;
+			this.primero.ant = nuevo;
+		}
+	}
+
+	buscar(nombre) {
+		if(this.primero === null) {
+			return null;
+		}
+		if(this.primero.ant.getNombre() === nombre) {
+			return this.primero.ant;
+		}
+
+		let aux = this.primero;
+		while(aux.sig !== this.primero) {
+			if(aux.getNombre() === nombre) {
+				return aux;
+			}
+			aux = aux.sig;
+		}
+		return null;
+	}
+	eliminar(nombre) {
+		if(this.primero === null) {
+			return false;
+		} else if(this.primero.sig === this.primero && this.primero.getNombre() === nombre) {
+			this.primero = null;
+			return true;
+		} else if(this.primero.getNombre() === nombre) {
+			this.primero.ant.sig = this.primero.sig;
+			this.primero.sig.ant = this.primero.ant;
+			this.primero = this.primero.sig;
+			return true;
+		} else {
+			let aux = this.primero;
+			while(aux.sig !== this.primero) {
+				if(aux.getNombre() === nombre) {
+					aux.ant.sig = aux.sig;
+					aux.sig.ant = aux.ant;
+					return true;
+				}
+				aux = aux.sig;
+			}
+		}
+		return false;
+	}
+	imprimir() {
+		let lista = ""
+		let aux = this.primero;
+		while(aux.sig !== this.primero) {
+			lista += aux.descripcion()
+			aux = aux.sig;
+		}
+		return lista;
+	}
+	//recorrido(baseInicio, horaInicio, minutoInicio, horaFin, minutoFin) {}
 }
+
+let ruta = new Ruta();
+let base = new Base('Rojos', 30);
+ruta.agregar(base);
+base = new Base('Comala', 50);
+ruta.agregar(base);
+base = new Base('Hospital', 60);
+ruta.agregar(base);
+base = new Base('Coliman', 20);
+ruta.agregar(base);
+base = new Base('Sorina', 15);
+ruta.agregar(base);
+//console.log(ruta.eliminar('Hospital'))
+console.log(ruta.imprimir());
